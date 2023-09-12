658046acf2d4daa42707af8edc5456703d921a1a
cjy9249 <cjy920409@gmail.com>
2023-09-12 23:39:55 +0900

    FEAT : Product 기능 구현

---
+++ src/main/java/com/example/kakao/product/ProductResponse.java
@@ -0,0 +1,24 @@
+package com.example.kakao.product;
+
+import com.example.kakao.option.Option;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ProductResponse {
+
+    @Getter @Setter
+    public static class FindAllDTO {
+
+
+    }
+
+
+    @Getter @Setter
+    public static class FindByIdDTO {
+
+
+    }
+}
